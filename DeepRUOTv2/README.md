d1e189b7f55a7d6009ea4ce25a56e83aaab5a5de
XueyaZhou-124 <1259467101@qq.com>
2026-03-06 15:23:39 +0800

    modified DeepRUOTv2 module

---
+++ DeepRUOTv2/README.md
@@ -0,0 +1,168 @@
+# DeepRUOTv2 (Dynamical Optimal Transport Tools)
+
+**Author**: Zhenyi Zhang, Zihan Wang
+
+This is the DeepRUOTv2 version of our previously published work [DeepRUOT](https://github.com/zhenyiizhang/DeepRUOT). We have improved the implementation of the original DeepRUOT version, offering a more user-friendly interface and establishing default parameters. We have computed results on more datasets presented in our latest work (https://arxiv.org/abs/2505.11197): Mouse Blood Hematopoiesis, Embryoid Body, Pancreatic $\beta$ -cell differentiation and  A549 EMT.
+
+Based on the DeepRUOTv2 framework, we implement a suite of dynamical optimal transport methods:
+
+- **Dynamical Optimal Transport (OT)**: classical formulation without growth/death processes or stochastic effects.
+- **Unbalanced Dynamical OT**: extension of dynamical OT that accounts for growth and death processes, but without stochastic effects.
+- **Dynamical Schrödinger Bridge (SB)**: stochastic extension of dynamical OT without growth/death.
+- **Regularized Unbalanced Optimal Transport (Unbalanced Schrödinger Bridge)**: general formulation that incorporates both growth/death processes and stochasticity.
+
+Users can flexibly specify the desired model through configuration files to select the appropriate solver for their application. Furthermore, the following downstream analysis can be conducted ([view tutorial](https://deepruot.readthedocs.io/en/latest/notebook/analysis.html)):
+<br />
+<div align="left">
+  <a href="https://github.com/zhenyiizhang/DeepRUOTv2/">
+    <img src="figures/DeepRUOT_github.png" alt="Logo" height="350">
+  </a>
+
+</div>
+
+If you are interested in further exploring cell-cell interactions from the data, we plan to release the code following the publication of our work.
+
+## Updated
+**(2025/08/26)** We conduct the following updates:
+* Added a new `use_mass` option to the configuration file. Setting this to `False` disables the growth term.
+* The `sigma` parameter can now be set to `0.0` to disable stochastic effects.
+* The calculated results can now be automatically evaluated.
+* Added a new notebook for downstream analysis at `evaluation/analysis.ipynb` ([view notebook](evaluation/analysis.ipynb), [view tutorial](https://deepruot.readthedocs.io/en/latest/notebook/analysis.html)). This script enables more advanced analyses, including data interpolation, inferring fate probabilities, and further gene-level studies. Raw data containing cell type and gene level information is required to perform these downstream analysis, sample data is provided in the following link ([view sample data](https://drive.google.com/drive/folders/1s2_hsS7WHVo03S4wgrn-w66iBtZPfdpS)).
+
+## Getting Started
+
+1. Clone this repository:
+
+```vim
+git clone https://github.com/zhenyiizhang/DeepRUOTv2
+```
+
+2. You can create a new conda environment (DeepRUOTv2) using
+
+```vim
+conda create -n DeepRUOTv2 python=3.10 ipykernel -y
+conda activate DeepRUOTv2
+```
+
+3. Install requirements
+```vim
+cd path_to_DeepRUOTv2
+pip install -r requirements.txt
+```
+
+## Training DeepRUOTv2
+
+DeepRUOTv2 uses a flexible configuration system, where users can specify the parameters used to train DeepRUOT. We provide example configurations used to train on four scRNA-seq datasets: Mouse Blood Hematopoiesis (50D), Embryoid Body (50D), Pancreatic $\beta$ -cell differentiation (30D) and  A549 EMT (10D). The configurations are stored in the `config/` folder.
+
+To train DeepRUOTv2 on your own dataset, you need to convert your own dataset to a csv file and store it in the `data/` folder. Specifically, the column `samples` refers to the biological time points starting from time 0, and it is recommended to normalize the time scales to a reasonable range. The following columns, starting from `x1`, refer to the gene expression features. After the dataset is prepared, modify these parts in the confuguration file:
+
+```yaml
+device: 'cuda' # device to run the model
+
+exp:
+  name: "my_experiment"     # Experiment name
+
+data:
+  file_path: "data.csv"     # Path to your dataset, your dataset should be prepared as a csv file
+  dim: 50                   # Data dimension
+
+model:
+  in_out_dim: 50 # Data dimension
+```
+
+For other hyperparameters, we recommend using the same settings as `config/weinreb_config.yaml`. Note that the default setting for the hyperparameter `use_pinn`, which controls whether to update the score model in the final training phase, is set to False. Setting it to True may achieve better performance but will significantly increase training time. For more efficient training, we recommend setting it to False. If you encounter  `CUDA out of memory` error, you may set the parameters `sample_size` and `score_batch_size` to smaller values.
+
+For training, simply specify the path to your configuration file, and run  `train_RUOT.py`:
+
+```bash
+python train_RUOT.py --config config/<config_name>.yaml
+```
+
+For example, to reproduce our results on the Mouse Blood Hematopoiesis dataset, run:
+
+```bash
+python train_RUOT.py --config config/weinreb_config.yaml
+```
+
+## Evaluation
+
+ After training, model checkpoints will be generated in the `results/` directory: `model_final` and `score_final`, which can then be used to inference trajectories. We provide a Jupyter notebook to plot the learned results in `evaluation/plot.ipynb`. Downstream analysis can be conducted using the provided notebook in `evaluation/analysis.ipynb` ([view tutorial](https://deepruot.readthedocs.io/en/latest/notebook/analysis.html)).
+
+## How to cite
+
+If you find DeepRUOT(v2) useful in your research, please consider citing our work.
+
+**DeepRUOT**:
+<details>
+<summary>
+Zhang, Z., Li, T., & Zhou, P. (2025). Learning stochastic dynamics from snapshots through regularized unbalanced optimal transport. The Thirteenth International Conference on Learning Representations.
+</summary>
+
+```bibtex
+@inproceedings{
+zhang2025learning,
+title={Learning stochastic dynamics from snapshots through regularized unbalanced optimal transport},
+author={Zhenyi Zhang and Tiejun Li and Peijie Zhou},
+booktitle={The Thirteenth International Conference on Learning Representations},
+year={2025},
+url={https://openreview.net/forum?id=gQlxd3Mtru}
+}
+```
+</details>
+
+**Review**:
+<details>
+<summary>
+Zhang, Z.; Sun, Y.; Peng, Q.; Li, T.; Zhou, P. Integrating Dynamical Systems Modeling with Spatiotemporal scRNA-Seq Data Analysis. Entropy 2025, 27, 453. https://doi.org/10.3390/e27050453
+
+</summary>
+
+```bibtex
+@Article{ZhangIntegrating2025,
+AUTHOR = {Zhang, Zhenyi and Sun, Yuhao and Peng, Qiangwei and Li, Tiejun and Zhou, Peijie},
+TITLE = {Integrating Dynamical Systems Modeling with Spatiotemporal scRNA-Seq Data Analysis},
+JOURNAL = {Entropy},
+VOLUME = {27},
+YEAR = {2025},
+NUMBER = {5},
+ARTICLE-NUMBER = {453},
+PubMedID = {40422408},
+ISSN = {1099-4300}
+}
+
+```
+</details>
+
+
+## Contact information
+
+If you encounter any issues while running the code, please feel free to contact us and we warmly welcome any discussions and suggestions:
+
+Zhenyi Zhang (zhenyizhang.math@gmail.com)
+
+## License
+DeepRUOTv2 is licensed under the MIT License.
+
+```
+License
+
+Copyright (c) 2025 Zhenyi Zhang and Zihan Wang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ DeepRUOTv2/infer_RUOT.py
@@ -0,0 +1,378 @@
+# 负责生成计算metirc需要的数据
+# sde_point_*.npy, sde_weight_*.npy
+
+# velocity.adata
+
+# pesudotime.adata
+
+# type: ignore
+
+# evaluate RUOT and visulazation
+import os
+import sys
+import argparse
+import random
+import math
+import joblib
+import numpy as np
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
+import anndata
+import scvelo as scv
+import scanpy as sc
+import umap
+from tqdm import tqdm
+from sklearn.decomposition import PCA
+import ot
+from torchdiffeq import odeint_adjoint as odeint
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
+
+from DeepRUOT.losses import OT_loss1
+from DeepRUOT.utils import (load_and_merge_config, euler_sdeint, euler_sdeint_reverse,)
+from DeepRUOT.models import FNet, scoreNet2
+from DeepRUOT.constants import DATA_DIR, RES_DIR
+from DeepRUOT.exp import setup_exp
+
+
+class SDE(torch.nn.Module):
+    noise_type = "diagonal"
+    sde_type = "ito"
+
+    def __init__(self, ode_drift, g, score, input_size=(3, 32, 32), sigma=1.0):
+        super().__init__()
+        self.drift = ode_drift
+        self.score = score
+        self.input_size = input_size
+        self.sigma = sigma
+        self.g_net = g
+
+    # Drift
+    def f(self, t, y):
+        z, lnw = y
+        drift=self.drift(t, z)
+        dlnw = self.g_net(t, z)
+        num = z.shape[0]
+        t = t.expand(num, 1)  # Keep gradient information of t and expand its shape
+        return (drift+self.score.compute_gradient(t, z), dlnw)
+
+    # Diffusion
+    def g(self, t, y):
+        return torch.ones_like(y)*self.sigma
+    
+
+class SDE_reverse(torch.nn.Module):
+    noise_type = "diagonal"
+    sde_type = "ito"
+
+    def __init__(self, ode_drift, g, score, input_size=(3, 32, 32), sigma=1.0):
+        super().__init__()
+        self.drift = ode_drift
+        self.score = score
+        self.input_size = input_size
+        self.sigma = sigma
+        self.g_net = g
+
+    # Drift
+    def f(self, t, y):
+        z, lnw = y
+        drift=self.drift(t, z)
+        dlnw = self.g_net(t, z)
+        num = z.shape[0]
+        t = t.expand(num, 1)  # Keep gradient information of t and expand its shape
+        if self.sigma == 0:
+            return (drift, dlnw)
+        else:
+            return (drift - self.score.compute_gradient(t, z), dlnw)
+
+    # Diffusion
+    def g(self, t, y):
+        return torch.ones_like(y)*self.sigma
+    
+
+def load_data_model(config, device):
+    """
+    加载数据和模型
+    """
+    df = pd.read_csv(os.path.join(DATA_DIR, config['data']['file_path']))
+    df = df.iloc[:, :config['data']['dim'] + 1]
+    exp_dir = os.path.join(config['exp']['output_dir'], config['exp']['name'])
+    model_config = config['model']
+    f_net = FNet(
+        in_out_dim=model_config['in_out_dim'],
+        hidden_dim=model_config['hidden_dim'],
+        n_hiddens=model_config['n_hiddens'],
+        activation=model_config['activation']
+    ).to(device)
+    sf2m_score_model = scoreNet2(
+        in_out_dim=model_config['in_out_dim'],
+        hidden_dim=model_config['score_hidden_dim'],
+        activation=model_config['activation']
+    ).float().to(device)
+    f_net.load_state_dict(torch.load(os.path.join(exp_dir, 'model_final'), map_location=device))
+    sf2m_score_model.load_state_dict(torch.load(os.path.join(exp_dir, 'score_model_final'), map_location=device))
+    return df, f_net, sf2m_score_model, exp_dir
+
+
+def generate_velocity(df, f_net, dim, exp_dir, device='cuda'):
+    """
+    generate velocity data
+    """
+    all_times = df['samples'].values
+    all_data = df[[f'x{i}' for i in range(1, dim + 1)]].values
+    # Step 2: Convert to PyTorch tensors
+    t_tensor = torch.tensor(all_times).unsqueeze(1).float().to(device)
+    data_tensor = torch.tensor(all_data, dtype=torch.float32).to(device)
+    data_tensor.requires_grad_(True)
+    gradients = f_net.v_net(t_tensor, data_tensor)
+    data_np = data_tensor.detach().cpu().numpy()
+    data_np_2d = data_np[:, :2]
+    
+    gradients_np = gradients.detach().cpu().numpy()
+    data_end = data_np + gradients_np
+
+    data_end_2d = data_end[:, :2]
+    gradients_np = data_end_2d - data_np_2d
+    gradients_np = gradients_np / np.linalg.norm(gradients_np, axis=1, keepdims=True) * 5
+    adata = anndata.AnnData(X=all_data)
+    adata.layers['Ms'] = all_data 
+    adata.obsm['X_PCA'] = data_np_2d
+    adata.obs['time'] = all_times
+    adata.layers['velocity'] = gradients.detach().cpu().numpy()
+
+    adata.write_h5ad(os.path.join(exp_dir, 'velocity.h5ad'))
+
+    return adata
+
+
+def generate_trajectory_point(df, f_net, sf2m_score_model, dim, exp_dir, sigma, initial_num_points = None, device = 'cuda', traject_sample_number = 100, num_runs = 5, use_mass = False, output_dir = None, file_prefix = "sde"):
+    """
+    generate simulated data, 
+        point data & weight data for dynamic metric calculation(W1, TMV), 
+        trajectory data & ts & lnw for pesudotime calculation
+    """
+    all_times = df['samples'].values
+    all_data = df[[f'x{i}' for i in range(1, dim + 1)]].values
+
+    n_times = all_times.max() + 1
+    data=torch.tensor(df[df['samples']==0].values,dtype=torch.float32).requires_grad_()
+    data_t0 = data[:, 1:].to(device).requires_grad_()
+    print(data_t0.shape)
+    x0=data_t0.to(device)
+    
+    # 从t0开始生成数据
+    results = []
+    for run_idx in tqdm(range(num_runs)):
+        SEED = run_idx
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.random.manual_seed(SEED)
+        if initial_num_points is not None:
+            sample_indices = random.sample(range(x0.size(0)), initial_num_points)
+            x0_subset = x0[sample_indices].to(device)
+        else:
+            x0_subset = x0.to(device)
+
+        lnw0 = torch.log(torch.ones(x0_subset.shape[0], 1) / x0_subset.shape[0]).to(device)
+        initial_state = (x0_subset, lnw0)
+
+        for param in f_net.parameters():
+            param.requires_grad = False
+        for param in sf2m_score_model.parameters():
+            param.requires_grad = False
+
+        # Define SDE object
+        sde = SDE(f_net.v_net, 
+                f_net.g_net, 
+                sf2m_score_model, 
+                input_size=(2,), 
+                sigma=sigma)
+
+        ts = torch.linspace(0, n_times - 1, 100, device=device)
+        sde_traj, traj_lnw = euler_sdeint(sde, initial_state, dt=0.1, ts=ts) # 从data_t0出发生成采样轨迹
+        sde_traj, traj_lnw = sde_traj.cpu(), traj_lnw.cpu()
+
+        sample_number = traject_sample_number  # 采样100条轨迹用于保存
+        sample_indices = random.sample(range(sde_traj.size(1)), sample_number)
+        sampled_sde_trajec = sde_traj[:, sample_indices, :]
+        sampled_sde_trajec = sampled_sde_trajec.tolist()
+        sampled_sde_trajec = np.array(sampled_sde_trajec, dtype=object)
+        np.save(os.path.join(exp_dir, f'sde_trajec_{run_idx}.npy'), sampled_sde_trajec)
+
+        ts_points = torch.tensor(sorted(df.samples.unique()), dtype=torch.float32)
+        sde_point, traj_lnw = euler_sdeint(sde, initial_state, dt=0.1, ts=ts_points)
+        if use_mass:
+            weight = torch.exp(traj_lnw)
+        else:
+            weight = torch.ones_like(traj_lnw)
+
+        sde_point_np = sde_point.detach().cpu().numpy()
+        sde_point_list = sde_point_np.tolist()
+        sde_point_array = np.array(sde_point_list, dtype=object)
+        np.save(os.path.join(exp_dir, f'sde_point_{run_idx}.npy'), sde_point_array) # 生成point数据
+        np.save(os.path.join(exp_dir, f'sde_weight_{run_idx}.npy'), weight.detach().cpu().numpy())
+
+        # 5.5 转为 numpy
+        out = {
+            "traj": sde_traj.detach().cpu().numpy(),  # (T,N,G)
+            "lnw": traj_lnw.detach().cpu().numpy(),   # (T,N,1)
+            "ts": ts.detach().cpu().numpy(),          # (T,)
+            "point": sde_point_array,  # (T,N,G)
+            "weight": weight.detach().cpu().numpy(),   # (T,N,1)
+        }
+        results.append(out)
+
+        # 5.6 可选保存
+        if output_dir is not None:
+            np.savez(
+                os.path.join(output_dir, f"{file_prefix}_run_{run_idx}.npz"),
+                traj=out["traj"],
+                lnw=out["lnw"],
+                ts=out["ts"],
+                point=out["point"],
+                weight=out["weight"]
+            )
+
+    return results
+
+
+def generate_trajectories_sde_hold_start_out(df, f_net, sf2m_score_model, device, exp_dir, all_times, sigma, use_mass, num_points=None, num_runs = 1, output_dir = None, file_prefix = "sde"):
+    n_times = np.array(all_times).max() + 1
+    data=torch.tensor(df[df['samples']==all_times[1]].values,dtype=torch.float32).requires_grad_() # t1 data
+    data_t1 = data[:, 1:].to(device).requires_grad_()
+    x1=data_t1.to(device)
+    results = []
+    for run_idx in tqdm(range(num_runs)):
+        SEED = run_idx
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.random.manual_seed(SEED)
+        if num_points is not None:
+            sample_indices = random.sample(range(x1.size(0)), num_points)
+            x1_subset = x1[sample_indices].to(device)
+        else:
+            x1_subset = x1.to(device)
+
+        lnw0 = torch.log(torch.ones(x1_subset.shape[0], 1) / x1_subset.shape[0] * (len(df[df['samples'] == all_times[1]]) / len(df[df['samples'] == all_times[0]]))).to(device)
+        initial_state = (x1_subset, lnw0)
+
+        for param in f_net.parameters():
+            param.requires_grad = False
+        for param in sf2m_score_model.parameters():
+            param.requires_grad = False
+
+        # Define SDE object
+        sde = SDE(f_net.v_net, 
+                f_net.g_net, 
+                sf2m_score_model, 
+                input_size=(2,), 
+                sigma=sigma)
+        sde_reverse = SDE_reverse(f_net.v_net, 
+                f_net.g_net, 
+                sf2m_score_model, 
+                input_size=(2,), 
+                sigma=sigma)
+        
+        ts_reverse = torch.linspace(all_times[1], all_times[0], 10, device=device)
+        sde_traj_reverse, traj_lnw_reverse = euler_sdeint_reverse(sde_reverse, initial_state, dt=0.1, ts=ts_reverse)
+        sde_traj_reverse, traj_lnw_reverse = sde_traj_reverse.cpu(), traj_lnw_reverse.cpu()
+
+        ts_forward = torch.linspace(all_times[1], n_times - 1, 100, device=device)
+        sde_traj_forward, traj_lnw_forward = euler_sdeint(sde, initial_state, dt=0.1, ts=ts_forward)
+        sde_traj_forward, traj_lnw_forward = sde_traj_forward.cpu(), traj_lnw_forward.cpu()
+
+        sde_traj = torch.cat([sde_traj_reverse.flip(0), sde_traj_forward[1:]], dim=0)
+        traj_lnw = torch.cat([traj_lnw_reverse.flip(0), traj_lnw_forward[1:]], dim=0)
+
+        sample_number = 100  # For example, sample 10
+        sample_indices = random.sample(range(sde_traj.size(1)), sample_number)
+        sampled_sde_trajec = sde_traj[:, sample_indices, :]
+        sampled_sde_trajec = sampled_sde_trajec.tolist()
+        sampled_sde_trajec = np.array(sampled_sde_trajec, dtype=object)
+        np.save(os.path.join(exp_dir, f'sde_trajec_{run_idx}.npy'), sampled_sde_trajec)
+
+        ts_points_reverse = torch.tensor([all_times[1], all_times[0]], dtype=torch.float32)
+        sde_point_0, traj_lnw_0 = euler_sdeint_reverse(sde_reverse, initial_state, dt=0.1, ts=ts_points_reverse)
+        if use_mass:
+            weight_0 = torch.exp(traj_lnw_0)
+        else:
+            weight_0 = torch.ones_like(traj_lnw_0)
+
+        sde_point_np_0 = sde_point_0[-1].unsqueeze(0).detach().cpu().numpy()
+        weight_0 = weight_0[-1].unsqueeze(0).detach().cpu().numpy()
+
+        ts_points_forward = torch.tensor([all_times[t] for t in range(1, len(all_times))], dtype=torch.float32)
+        print(ts_points_forward)
+        sde_point_forward, traj_lnw_forward = euler_sdeint(sde, initial_state, dt=0.1, ts=ts_points_forward)
+        if use_mass:
+            weight_forward = torch.exp(traj_lnw_forward)
+        else:
+            weight_forward = torch.ones_like(traj_lnw_forward)
+
+        sde_point_np_forward = sde_point_forward.detach().cpu().numpy()
+        weight_forward = weight_forward.detach().cpu().numpy()
+        sde_point_np = np.concatenate([sde_point_np_0, sde_point_np_forward], axis=0)
+        print(sde_point_np.shape)
+        weight = np.concatenate([weight_0, weight_forward], axis=0)
+        sde_point_list = sde_point_np.tolist()
+        sde_point_array = np.array(sde_point_list, dtype=object)
+        np.save(os.path.join(exp_dir, f'sde_point_{run_idx}.npy'), sde_point_array)
+        np.save(os.path.join(exp_dir, f'sde_weight_{run_idx}.npy'), weight)
+        # 5.5 转为 numpy
+        # 把reverse和forward的traj和lnw和ts拼接起来
+        ts = torch.cat([ts_reverse.flip(0), ts_forward[1:]], dim=0)
+        out = {
+            "traj": sde_traj.detach().cpu().numpy(),  # (T,N,G)
+            "lnw": traj_lnw.detach().cpu().numpy(),   # (T,N,1)
+            "ts": ts.detach().cpu().numpy(),          # (T,)
+            "point": sde_point_array,  # (T,N,G)
+            "weight": weight,   # (T,N,1)
+        }
+        results.append(out)
+
+        # 5.6 可选保存
+        if output_dir is not None:
+            np.savez(
+                os.path.join(output_dir, f"{file_prefix}_run_{run_idx}.npz"),
+                traj=out["traj"],
+                lnw=out["lnw"],
+                ts=out["ts"],
+                point=out["point"],
+                weight=out["weight"]
+            )
+    return results
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Train DeepRUOT model')
+    parser.add_argument('--config', type=str, required=True, help='Path to config file')
+    parser.add_argument('--num_runs', type=int, required=True, help='Path to config file')
+
+    args = parser.parse_args()
+    # 设备选择
+    device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    # 加载配置、数据、模型
+    config = load_and_merge_config(args.config)
+    print(config)
+    df, f_net, sf2m_score_model, exp_dir = load_data_model(config, device)
+    dim = config['data']['dim']
+    num_runs = args.num_runs
+    # 进行inference，生成metirc所需数据并保存
+    sigma = config['score_train']['sigma']
+    use_mass = config['use_mass']
+    # 生成velocity数据
+    adata = generate_velocity(df, f_net, dim, exp_dir, device=device)
+    # 生成trajectory数据
+    if config['data']['hold_one_out'] & (config['data']['hold_out'] == 0):
+        all_times = sorted(df.samples.unique())
+        results_hold_start_out = generate_trajectories_sde_hold_start_out(df, f_net, sf2m_score_model, device, exp_dir, all_times, sigma, use_mass, num_points=None, num_runs = num_runs, output_dir = exp_dir)
+    else:
+        results = generate_trajectory_point(df, f_net, sf2m_score_model, dim, exp_dir, initial_num_points = None, sigma=sigma, use_mass = use_mass, device = device, traject_sample_number = 100, num_runs = num_runs, output_dir = exp_dir)
+    print("Inference completed and results saved.")
+
+if __name__ == '__main__':
+    main() 
+
+
